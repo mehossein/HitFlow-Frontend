b5a8e9a3382b31489ec2731857c14feb1e3a227f
Sepandar.d <sepandar.derakhshandeh@gmail.com>
2022-08-31 21:05:36 +0430

    feat(core) add responsive service

--- project/src/app/core/layout/responsive-layout.service.ts
+++ project/src/app/core/layout/responsive-layout.service.ts
@@ -1,9 +1,77 @@
-import { Injectable } from '@angular/core';
-
+import { isPlatformServer } from '@angular/common';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Inject, Injectable, Optional, PLATFORM_ID } from '@angular/core';
+import { combineLatest, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ResponsiveLayoutService {
+  // basic
+  isXSmallScreen: Observable<boolean>;
+  isSmallScreen: Observable<boolean>;
+  isMediumScreen: Observable<boolean>;
+  isLargeScreen: Observable<boolean>;
+  isXLargeScreen: Observable<boolean>;
+  // derived
+  columnCount: Observable<number>;
+  isSmallOrSmaller: Observable<boolean>;
+  isLargeOrBigger: Observable<boolean>;
+  isSmallOrSmallerSync: boolean;
+  // server
+  isServerMobile = false;
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: any,
+    private breakpointObserver: BreakpointObserver
+  ) {
+ 
 
-  constructor() { }
+    this.isSmallOrSmallerSync =
+      this.breakpointObserver.isMatched(Breakpoints.XSmall) ||
+      this.breakpointObserver.isMatched(Breakpoints.Small);
+    this.isXSmallScreen = this.breakpointObserver
+      .observe([Breakpoints.XSmall])
+      .pipe(map((result) => result.matches));
+    this.isSmallScreen = this.breakpointObserver
+      .observe([Breakpoints.Small])
+      .pipe(map((result) => result.matches));
+    this.isMediumScreen = this.breakpointObserver
+      .observe([Breakpoints.Medium])
+      .pipe(map((result) => result.matches));
+    this.isLargeScreen = this.breakpointObserver
+      .observe([Breakpoints.Large])
+      .pipe(map((result) => result.matches));
+    this.isXLargeScreen = this.breakpointObserver
+      .observe([Breakpoints.XLarge])
+      .pipe(map((result) => result.matches));
+    this.columnCount = combineLatest([
+      this.isXSmallScreen,
+      this.isSmallScreen,
+      this.isMediumScreen,
+      this.isLargeScreen,
+    ]).pipe(
+      map(([isXSmall, isSmall, isMedium, isLarge]) => {
+        if (isPlatformServer(this.platformId)) {
+          return this.isServerMobile ? 1 : 3;
+        } else {
+          return isXSmall ? 1 : isSmall ? 2 : isMedium ? 2 : isLarge ? 3 : 4;
+        }
+      })
+    );
+    this.isSmallOrSmaller = this.breakpointObserver
+      .observe([Breakpoints.XSmall, Breakpoints.Small])
+      .pipe(
+        map((result) => {
+          if (isPlatformServer(this.platformId)) {
+            return this.isServerMobile;
+          } else {
+            return result.matches;
+          }
+        })
+      );
+    this.isLargeOrBigger = this.breakpointObserver
+      .observe([Breakpoints.Large, Breakpoints.XLarge])
+      .pipe(map((result) => result.matches));
+  }
 }
+
