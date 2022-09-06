341948755426472dd78f71290e8541f434e75632
Sepandar.d <Sepandar.derakhshandeh@gmail.com>
2022-09-06 21:05:19 +0430

    WIP

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -1,22 +1,21 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from '@core/core.module';
 import { LoadingInterceptor } from '@core/interceptor';
 import { LoadingService } from '@core/service';
-import { ToastrModule } from 'ngx-toastr';
 import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-
+    SharedModule,
     CoreModule.forRoot({
       environment: environment,
     }),
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   content: ["./project/src/**/*.{html,ts}"],
   theme: {
     extend: {},
+    colors: {
+      background: "#191414",
+      main: "#ffffff",
+    },
+    fontFamily: {
+      brand: ["Russo One", "sans-serif"],
+      body: 'Poppins',
+    },
   },
   plugins: [],
 };
