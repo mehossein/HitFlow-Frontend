128410077f76b9c5d45f2f5d7ebd89c87ae3bef7
Sepandar.d <Sepandar.derakhshandeh@gmail.com>
2022-09-10 18:37:21 +0430

    feat(feature) add routing module

--- project/src/app/feature/feature.module.ts
+++ project/src/app/feature/feature.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { FeatureRoutingModule } from './feature-routing.module';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LandingPageComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    FeatureRoutingModule
   ]
 })
 export class FeatureModule { }
