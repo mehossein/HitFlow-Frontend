0ae85a13db7fd60af7619992eb413d60e10b080e
Sepandar.d <sepandar.derakhshandeh@gmail.com>
2022-09-16 23:26:28 +0430

    feat(style) add box shadow to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./project/src/**/*.{html,ts}"], 
-  theme: { 
-    extend: {}, 
-    colors: { 
-      main: "#ffffff", 
+  content: ["./project/src/**/*.{html,ts}"],
+  theme: {
+    extend: {},
+    colors: {
+      background: '#f7f7fa',
+      main: "#ffffff",
       nav: "#28293D",
       primary: "#3E7BFA",
       "primary-hover": "#6698ff",
       "primary-focus": "#3568df",
-    }, 
-    fontFamily: { 
+    },
+    fontFamily: {
       brand: ["Russo One", "sans-serif"],
       body: "Poppins",
-    }, 
-  }, 
-  plugins: [require("daisyui")], 
- 
-  daisyui: { 
+    },
+    boxShadow: {
+      nav: "0px 0px 2px rgba(40, 41, 61, 0.04), 0px 4px 8px rgba(96, 97, 112, 0.16);",
+    },
+  },
+  plugins: [require("daisyui")],
+
+  daisyui: {
     themes: ["light"],
   },
 };
