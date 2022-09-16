54a86468c7e4566cf6e55d4a32ca8b0a5e7d3535
Sepandar.d <sepandar.derakhshandeh@gmail.com>
2022-09-16 23:28:04 +0430

    feat(shared) change indexing and add hitFlow card component

--- project/src/app/shared/components/index.ts
+++ project/src/app/shared/components/index.ts
@@ -1,1 +1,5 @@
-export * from './navbar/navbar.component'
+import { Provider } from '@angular/core';
+import { CardComponent } from './card/card.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+export const COMPONENTS: Provider[] = [NavbarComponent, CardComponent];
