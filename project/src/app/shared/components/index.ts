fd4655cec8cbc89be9874f7c37099686c6d03895
Sepandar.d <sepandar.derakhshandeh@gmail.com>
2022-09-20 20:34:46 +0430

    feat(shared) add Skeleton components to project

--- project/src/app/shared/components/index.ts
+++ project/src/app/shared/components/index.ts
@@ -2,5 +2,13 @@ import { Provider } from '@angular/core';
 import { ButtonComponent } from './button/button.component';
 import { CardComponent } from './card/card.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SkeletonCardComponent } from './skeleton/skeleton-card/skeleton-card.component';
+import { SkeletonTextComponent } from './skeleton/skeleton-text/skeleton-text.component';
 
-export const COMPONENTS: Provider[] = [NavbarComponent, CardComponent,ButtonComponent];
+export const COMPONENTS: Provider[] = [
+  NavbarComponent,
+  CardComponent,
+  ButtonComponent,
+  SkeletonTextComponent,
+  SkeletonCardComponent
+];
